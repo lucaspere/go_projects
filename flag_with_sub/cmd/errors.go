669d38dc2af8c331ea00c193ec9efc84b104e9aa
lucaspere <lucasrupestre@hotmail.com>
2022-12-19 22:27:44 -0300

    feat: create initial cmds

---
+++ flag_with_sub/cmd/errors.go
@@ -0,0 +1,5 @@
+package cmd
+
+import "errors"
+
+var ErrNoServerSpecified = errors.New("Teste")
---
+++ flag_with_sub/cmd/grpc.go
@@ -0,0 +1,7 @@
+package cmd
+
+import "io"
+
+func HandleGrpc(w io.Writer, args []string) error {
+	return nil
+}
