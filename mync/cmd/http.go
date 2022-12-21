50bb26a3b4c1dc1690b6fcc9f89a2a4c06357a8c
lucaspere <lucasrupestre@hotmail.com>
2022-12-21 15:19:31 -0300

    feat: accepts multiparm content type

--- mync/cmd/http.go
+++ mync/cmd/http.go
@@ -6,17 +6,20 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type httpConfig struct {
-	url          string
-	verb         string
-	filePath     string
-	body         string
-	bodyFilePath string
+	url            string
+	verb           string
+	outputFilePath string
+	body           string
+	filePath       string
+	formData       map[string]string
 }
 
 func HandleHttp(w io.Writer, args []string) error {
@@ -25,9 +28,17 @@ func HandleHttp(w io.Writer, args []string) error {
 
 	fs.SetOutput(w)
 	fs.StringVar(&hc.verb, "verb", "GET", "HTTP method")
-	fs.StringVar(&hc.filePath, "output", "", "File path where save the data's output")
+	fs.StringVar(&hc.outputFilePath, "output", "", "File path where save the data's output")
 	fs.StringVar(&hc.body, "body", "", "JSON data to be used as payload")
-	fs.StringVar(&hc.bodyFilePath, "body-file", "", "File path containing the JSON data to be used as payload")
+	fs.StringVar(&hc.filePath, "body-file", "", "File path containing the JSON data to be used as payload")
+	fs.StringVar(&hc.filePath, "upload", "", "File path of the upload file")
+	str := fs.String("form-data", "", "Form-Data key-value pair")
+
+	if len(*str) > 0 {
+		form := strings.Split(*str, "=")
+
+		hc.formData[form[0]] = form[1]
+	}
 
 	fs.Usage = func() {
 		var usageString = `
@@ -51,10 +62,10 @@ http: <options> server`
 	hc.url = fs.Arg(0)
 	fmt.Fprintln(w, "Executing http command")
 
-	if len(hc.filePath) > 0 {
+	if len(hc.outputFilePath) > 0 {
 		var file *os.File
 
-		path := filepath.Join(hc.filePath)
+		path := filepath.Join(hc.outputFilePath)
 		file, err = os.Open(path)
 		if err != nil {
 			file, err = os.Create(path)
@@ -106,9 +117,10 @@ func (hc *httpConfig) handleGet() ([]byte, error) {
 
 func (hc *httpConfig) handlePost() ([]byte, error) {
 	var body io.Reader
-	if len(hc.bodyFilePath) > 0 {
-		path := filepath.Join(hc.bodyFilePath)
-		file, err := os.Open(path)
+	var ct = "application/json"
+	if len(hc.filePath) > 0 {
+		hc.filePath = filepath.Join(hc.filePath)
+		file, err := os.Open(hc.filePath)
 		if err != nil {
 			return nil, err
 		}
@@ -118,6 +130,12 @@ func (hc *httpConfig) handlePost() ([]byte, error) {
 			return j, err
 		}
 
+		if len(hc.formData) > 0 {
+			j, ct, err = hc.handleMultipart(j)
+			if err != nil {
+				return j, err
+			}
+		}
 		if !json.Valid(j) {
 			return nil, InvalidJsonBody
 		}
@@ -132,11 +150,42 @@ func (hc *httpConfig) handlePost() ([]byte, error) {
 		body = bytes.NewReader([]byte(hc.body))
 	}
 
-	res, err := http.Post(hc.url, "application/json", body)
+	res, err := http.Post(hc.url, ct, body)
 	if err != nil {
 		return nil, err
 	}
 
 	defer res.Body.Close()
 	return io.ReadAll(res.Body)
+}
+
+func (hc *httpConfig) handleMultipart(b []byte) ([]byte, string, error) {
+	var buf bytes.Buffer
+	var err error
+	var fw io.Writer
+
+	mw := multipart.NewWriter(&buf)
+
+	for key, value := range hc.formData {
+		fw, err = mw.CreateFormField(key)
+		if err != nil {
+			return nil, "", err
+		}
+
+		fmt.Fprintf(fw, value)
+	}
+
+	fw, err = mw.CreateFormFile("filedata", hc.filePath)
+	if err != nil {
+		return nil, "", err
+	}
+	data := bytes.NewReader(b)
+	_, err = io.Copy(fw, data)
+	err = mw.Close()
+	if err != nil {
+		return nil, "", err
+	}
+
+	contentType := mw.FormDataContentType()
+	return buf.Bytes(), contentType, nil
 }
