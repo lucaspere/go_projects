7c4581fca9aa515ae1de3dd5b9bfa68744500fd0
lucaspere <lucasrupestre@hotmail.com>
2023-01-03 22:06:42 -0300

    tests: create tests to validations

--- greeter-cmd-app/main_test.go
+++ greeter-cmd-app/main_test.go
@@ -5,15 +5,13 @@ import (
 	"testing"
 )
 
-type testConfig struct {
-	args           []string
-	expectedErr    error
-	expectedConfig config
-}
-
 func TestParseArgs(t *testing.T) {
 	var c config
-	tests := []testConfig{
+	tests := []struct {
+		args           []string
+		expectedErr    error
+		expectedConfig config
+	}{
 		{
 			args:           []string{},
 			expectedErr:    errors.New("must specify one argument"),
@@ -40,6 +38,7 @@ func TestParseArgs(t *testing.T) {
 			expectedConfig: config{numTimes: 2},
 		},
 	}
+
 	for _, tc := range tests {
 		err := c.parseArgs(tc.args)
 
@@ -53,5 +52,63 @@ func TestParseArgs(t *testing.T) {
 			t.Errorf("Expected numTimes to be: %v, got: %v\n", tc.expectedConfig.numTimes, c.numTimes)
 		}
 	}
+}
 
+func TestValidateNumberArgs(t *testing.T) {
+	tests := []struct {
+		args     []string
+		expected error
+	}{
+		{
+			args:     []string{},
+			expected: errors.New("must specify one argument"),
+		},
+		{
+			args:     []string{"-n"},
+			expected: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		err := validateNumberArgs(tc.args)
+		if err != nil && tc.expected.Error() != err.Error() {
+			t.Errorf("Expected error to be: %v, got: %v\n", tc.expected, err)
+		}
+		if tc.expected == nil && err != nil {
+			t.Errorf("Expected nil error, got: %v\n", err)
+		}
+	}
+}
+
+func TestValidateArgs(t *testing.T) {
+	tests := []struct {
+		args     []string
+		expected error
+	}{
+		{
+			args:     []string{"-h"},
+			expected: errors.New("invalid -h argument. Must specify a valid argument -n"),
+		},
+		{
+			args:     []string{"-n"},
+			expected: errors.New("must insert some int value to -n argument"),
+		},
+		{
+			args:     []string{"-n", "2"},
+			expected: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		err := validateArgs(tc.args)
+		if err != nil && tc.expected.Error() != err.Error() {
+			t.Errorf("expected error to be: %v, got: %v\n", tc.expected, err)
+		}
+		if len(tc.args) == 1 && tc.expected.Error() != err.Error() {
+			t.Errorf("expected error to be: %v, got: %v\n", tc.expected, err)
+		}
+		if tc.expected == nil && err != nil {
+			t.Errorf("Expected nil error, got: %v\n", err)
+		}
+	}
 }
