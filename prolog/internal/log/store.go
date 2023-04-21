17118f335898c4b3133fac0e78d92b578a5aef39
lucaspere <lucasrupestre@hotmail.com>
2023-04-21 19:49:17 -0300

    feat(prolog): create the store file

---
+++ prolog/internal/log/store.go
@@ -0,0 +1,117 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+// Store represents a log file that can be appended to and read from.
+//
+// Embedding the `*os.File` pointer in the struct allows the `store` struct to access all the fields and methods of the `os.File` type, effectively inheriting its functionaly.
+// This makes it easy to use function from the `os` package to interact with the file, while also adding additional functionality for appending and reading data.
+//
+// The `sync.Mutex` is used to ensure that only one goroutine can access the file at a time, preventing concurrent writes from interfering with each other.
+// The `*bufio.Writer` is used to buffer writes to the file, which can improve performance by reducing the number of system calls needed to write data.
+// The `size` fiel represents the current size of the file.
+type store struct {
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
+
+// The `newStore` creates a `store` based on `*os.File` which represents a file that has already been opened.
+//
+// It returns a pointer to a `store` or a error if occurs.
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(fi.Size())
+
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
+}
+
+// The `Append` method is used to write data to the end of the store.
+// It takes a byte slice `p` as its argument, which represents the data to be written.
+// It returns the number of bytes written (`n`), the position in the file where the data was written (`pos`), and any errors that occurred during the write (`err`)/
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	pos = s.size
+	if err = binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return
+	}
+
+	nn, err := s.buf.Write(p)
+	if err != nil {
+		return n, pos, err
+	}
+
+	n = lenWidth + uint64(nn)
+	s.size += uint64(n)
+
+	return
+}
+
+// Read method reads data from the store at the specified position (`pos`).
+// It returns the buffer that hold the data. If there is an error reading the data, the method returns it.
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// The ReadAt reads data from the store at the specified offset.
+// It takes a byte slice p and an off argument as the offset from which to read the data.
+// It returns the number of bytes read and any error that occurred.
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+
+	return s.File.ReadAt(p, off)
+}
+
+// The Close method closes the file that is represented by the store.
+// Before closed it, the method flushes the buffer to ensure that any data in the buffer is written to the fie.
+// It returns a error if occurs during the closed.
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return err
+	}
+
+	return s.File.Close()
+}
---
+++ prolog/internal/log/store_test.go
@@ -0,0 +1,134 @@
+package log
+
+import (
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write)) + lenWidth
+)
+
+// func TestStoreAppendRead(t *testing.T) {
+
+// }
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_append_rest_test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+
+	store, err := newStore(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Testing Append", func(t *testing.T) {
+		t.Helper()
+
+		for i := uint64(1); i < 4; i++ {
+			n, pos, err := store.Append(write)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if pos+n != width*i {
+				t.Errorf("Expect %d to be equal %d", pos+n, width*i)
+			}
+		}
+	})
+	t.Run("Testing Read", func(t *testing.T) {
+		t.Helper()
+		var pos uint64
+
+		for i := uint64(1); i < 4; i++ {
+			read, err := store.Read(pos)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(read, write) {
+				t.Errorf("Expect %v to be equal %v", read, write)
+			}
+		}
+	})
+
+	t.Run("Testing ReadAt", func(t *testing.T) {
+		t.Helper()
+		for i, off := uint64(1), int64(0); i < 4; i++ {
+			b := make([]byte, lenWidth)
+			n, err := store.ReadAt(b, off)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if n != lenWidth {
+				t.Errorf("Expect %v to be equal %v", n, lenWidth)
+			}
+			off += int64(n)
+
+			size := enc.Uint64(b)
+			b = make([]byte, size)
+			n, err = store.ReadAt(b, off)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(write, b) {
+				t.Errorf("Expect %v to be equal %v", write, b)
+			}
+			if int(size) != n {
+				t.Errorf("Expect %v to be equal %v", int(size), lenWidth)
+			}
+			off += int64(n)
+		}
+	})
+}
+
+func TestStoreClose(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_close_test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+	s, err := newStore(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, _, err = s.Append(write)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f, beforeSize, err := openFile(f.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = s.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, afterSize, err := openFile(f.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if afterSize < beforeSize {
+		t.Errorf("Expect %v to be greater than %v", afterSize, beforeSize)
+	}
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, fi.Size(), nil
+}
