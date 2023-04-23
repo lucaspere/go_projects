6ea2bb51a222896a8cc148a97445e4e9d37cfbb8
lucaspere <lucasrupestre@hotmail.com>
2023-04-22 21:25:16 -0300

    feat(prolog): implement the log grpc methods

--- prolog/go.mod
+++ prolog/go.mod
@@ -3,11 +3,20 @@ module github.com/lucaspere/go_projects/proglog
 go 1.20
 
 require (
+	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.8.2
+	github.com/tysonmote/gommap v0.0.2
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1
+	google.golang.org/grpc v1.54.0
+	google.golang.org/protobuf v1.30.0
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
-	github.com/tysonmote/gommap v0.0.2 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ prolog/internal/server/server.go
@@ -0,0 +1,114 @@
+package server
+
+import (
+	"context"
+
+	api "github.com/lucaspere/go_projects/proglog/api/v1"
+	"google.golang.org/grpc"
+)
+
+type CommitLog interface {
+	Append(*api.Record) (uint64, error)
+	Read(uint64) (*api.Record, error)
+}
+
+type Config struct {
+	CommitLog
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+
+	return srv, nil
+}
+
+// NewGRPCServer creates a `grpcServer` pointer with the `Config`.
+func NewGRPCServer(config *Config) (*grpc.Server, error) {
+	gsrv := grpc.NewServer()
+	srv, err := newgrpcServer(config)
+	if err != nil {
+		return nil, err
+	}
+	api.RegisterLogServer(gsrv, srv)
+	return gsrv, nil
+}
+
+// Produce append to the log the record received.
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+// Consume read the offset's record and response it.
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
+	*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
+
+// ProduceStream receives a stream of records, append to the log, and response the its offset.
+func (s *grpcServer) ProduceStream(
+	stream api.Log_ProduceStreamServer,
+) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		res, err := s.Produce(stream.Context(), req)
+		if err != nil {
+			return err
+		}
+		if err = stream.Send(res); err != nil {
+			return err
+		}
+	}
+}
+
+// ConsumeStream receives a record `offset` and response with the record.
+// The server keeps the server open to waiting new append records to the log and streaming back to the client.
+//
+// It continues consuming subsequent `offset` records until:
+// - The client closed the connection;
+// - A error occurs.
+func (s *grpcServer) ConsumeStream(
+	req *api.ConsumeRequest,
+	stream api.Log_ConsumeStreamServer,
+) error {
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		default:
+			res, err := s.Consume(stream.Context(), req)
+			switch err.(type) {
+			case nil:
+			case api.ErrOffsetOutOfRange:
+				continue
+			default:
+				return err
+			}
+			if err = stream.Send(res); err != nil {
+				return err
+			}
+		}
+
+		req.Offset++
+	}
+}
