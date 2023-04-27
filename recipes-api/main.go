579f36ed2721b5df9164f2965d51936d7566fd94
lucaspere <lucasrupestre@hotmail.com>
2023-04-27 17:07:45 -0300

    feat(recipes-api): delete recipe endpoint

--- recipes-api/main.go
+++ recipes-api/main.go
@@ -24,6 +24,8 @@ func main() {
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
+
 	router.Run()
 }
 func NewRecipeHandler(c *gin.Context) {
@@ -73,6 +75,28 @@ func UpdateRecipeHandler(c *gin.Context) {
 	recipes[index] = recipe
 
 	c.JSON(http.StatusOK, recipe)
+}
+
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found"})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted",
+	})
 }
 
 type Recipe struct {
