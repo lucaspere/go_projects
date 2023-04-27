a3124c465d24923bc2b608972d56af0d055d0302
lucaspere <lucasrupestre@hotmail.com>
2023-04-27 11:55:11 -0300

    feat(recipes-api): recipe data model

--- recipes-api/main.go
+++ recipes-api/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 	router.Run()
+}
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
