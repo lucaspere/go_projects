c6d8fd26a3b2e1ff7d837423eb224cb25fa4769f
lucaspere <lucasrupestre@hotmail.com>
2023-04-27 12:23:27 -0300

    feat(recipes-api): update recipe endpoint

--- recipes-api/main.go
+++ recipes-api/main.go
@@ -23,7 +23,7 @@ func main() {
 
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
-
+	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.Run()
 }
 func NewRecipeHandler(c *gin.Context) {
@@ -46,6 +46,33 @@ func ListRecipesHandler(c *gin.Context) {
 
 	c.JSON(http.StatusOK, recipes)
 
+}
+
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found"})
+		return
+	}
+
+	recipes[index] = recipe
+
+	c.JSON(http.StatusOK, recipe)
 }
 
 type Recipe struct {
