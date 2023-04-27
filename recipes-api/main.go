2862a0174edbc1fd83b5a7812a80f8a1e8100774
lucaspere <lucasrupestre@hotmail.com>
2023-04-27 12:09:32 -0300

    feat(recipes-api): new recipe endpoint

--- recipes-api/main.go
+++ recipes-api/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
+
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
 
 func main() {
 	router := gin.Default()
+
+	router.POST("/recipes", NewRecipeHandler)
 	router.Run()
+}
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+
+	c.JSON((http.StatusOK), recipe)
 }
 
 type Recipe struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
