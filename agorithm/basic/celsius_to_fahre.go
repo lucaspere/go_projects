3910dea7fdc5cfad34d709471e05e0cca17f19a2
lucaspere <lucasrupestre@hotmail.com>
2023-05-27 18:59:40 -0300

    feat(algorithm: make the ConvertCtoF

---
+++ agorithm/basic/celsius_to_fahre.go
@@ -0,0 +1,6 @@
+package basic
+
+func ConvertCtoF(celsius float64) (fahrenheit float64) {
+	fahrenheit = (celsius * 9 / 5) + 32
+	return
+}
---
+++ agorithm/basic/celsius_to_fahre_test.go
@@ -0,0 +1,34 @@
+package basic
+
+import (
+	"testing"
+)
+
+func TestConversion(t *testing.T) {
+	tests := []struct {
+		input  float64
+		expect float64
+	}{{
+		input:  0,
+		expect: 32,
+	}, {
+		input:  -30,
+		expect: -22,
+	}, {
+		input:  -10,
+		expect: 14,
+	}, {
+		input:  20,
+		expect: 68,
+	}, {
+		input:  30,
+		expect: 86,
+	}}
+
+	for _, test := range tests {
+		fah := ConvertCtoF(test.input)
+		if test.expect != fah {
+			t.Errorf("Expect the Fahrenheit to be %f, Got %f", test.expect, fah)
+		}
+	}
+}
